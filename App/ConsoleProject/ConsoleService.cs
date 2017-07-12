bfdd36be04c21adde96b958a646481380c789bcf
VV <VV@vv>
2017-07-13 01:24:04 +0500

    refactored ConsoleService

--- App/ConsoleProject/ConsoleService.cs
+++ App/ConsoleProject/ConsoleService.cs
@@ -13,7 +13,9 @@ namespace App.ConsoleService
                 new Dictionary<string, Action<IEnumerable<string>>>
                 {
                     { "help", ExecuteCommand(() => { Console.WriteLine(File.ReadAllText("\\help.txt")); })},
+
                     { "clear", ExecuteCommand(Console.Clear) },
+
                     { "exit", ExecuteCommand(() => Environment.Exit(0))},
 
                     { "add", ExecuteCommand(() => Console.Clear())  },
@@ -21,41 +23,6 @@ namespace App.ConsoleService
 
                     { "post", ExecuteCommand(() => Console.Clear())  },
                 };
-
-            public static string ReadPassword()
-            {
-                string password = "";
-                ConsoleKeyInfo info = Console.ReadKey(true);
-
-                while (info.Key != ConsoleKey.Enter)
-                {
-                    if (info.Key != ConsoleKey.Backspace)
-                    {
-                        Console.Write("*");
-                        password += info.KeyChar;
-                    }
-                    else if (info.Key == ConsoleKey.Backspace)
-                    {
-                        if (!string.IsNullOrEmpty(password))
-                        {
-                            // remove one character from the list of password characters
-                            password = password.Substring(0, password.Length - 1);
-                            // get the location of the cursor
-                            int pos = Console.CursorLeft;
-                            // move the cursor to the left by one character
-                            Console.SetCursorPosition(pos - 1, Console.CursorTop);
-                            // replace it with space
-                            Console.Write(" ");
-                            // move the cursor to the left by one character again
-                            Console.SetCursorPosition(pos - 1, Console.CursorTop);
-                        }
-                    }
-                    info = Console.ReadKey(true);
-                }
-                // add a new line because user pressed enter at the end of their password
-                Console.WriteLine();
-                return password;
-            }
 
             public static List<string> ReadLogins()
             {
@@ -74,12 +41,6 @@ namespace App.ConsoleService
                 };
             }
 
-            IEnumerable<string> SplitIntoTokens(string s)
-            {
-                // тут надо бы что-то похитрее, как минимум учитывать группировку
-                // кавычками и escape-символы
-                return s.Split(null as char[], StringSplitOptions.RemoveEmptyEntries);
-            }
         }
 
 
