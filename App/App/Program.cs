66f1827727f81746d00ac21bfc653df9f73759e8
VV <VV@vv>
2017-07-13 01:25:23 +0500

    added test Program

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using App.SocialNetworkService;
+using App.StatisticService;
+using Newtonsoft.Json;
 
 namespace App
 {
     class Program
     {
+        private List<string> usernames = new List<string>();
+
         static void Main(string[] args)
         {
+            ISocialNetworkService networkService = new TwitterService();
+            networkService.AppOAuth();
+            var result = TextStatisticService.GetLetterFrequency
+                (String.Join
+                    (String.Empty, networkService.GetLastEntries("AsWeslyG", 5)));
+
+            var json = JsonConvert.SerializeObject(result, Formatting.None);
+
+            var str = new StringBuilder();
+            str.Append("@AsWeslyG");
+            str.Append(", статистика для последних 5 твитов: ");
+            str.Append(json);
+            str.Length = 140;
+
+            networkService.UserOAuth();
+            networkService.PostEntry(str.ToString());
+
+            Console.ReadKey();
         }
+
     }
+
 }
