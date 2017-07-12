aaf9e2c54ecc90a808f5cbbaf638ed365618e904
VV <VV@vv>
2017-07-12 13:55:27 +0500

    added class TextStatisticService and  function GetLetterFrequency to StatisticService

--- App/MessageStatisticProject/Program.cs
+++ App/MessageStatisticProject/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace App.Statistic
+namespace App.StatisticService
 {
     class Program
     {
---
+++ App/MessageStatisticProject/TextStatisticService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace App.StatisticService
+{
+    class TextStatisticService
+    {
+        public static Dictionary<char, float> GetLetterFrequency(string text)
+        {
+            if (text == null || text.Equals(String.Empty)) return null;
+
+            var result = new Dictionary<char, float>();
+
+            var regex = new Regex("[\\d\\W]");
+            var chars = regex.Replace(text, string.Empty).ToCharArray();
+
+            float addition = 1 / (float)chars.Length;
+
+            foreach (var character in chars)
+            {
+                if (!result.ContainsKey(character))
+                {
+                    result[character] = 0;
+                }
+
+                result[character] += addition;
+            }
+
+            return result;
+        }
+    }
+}
