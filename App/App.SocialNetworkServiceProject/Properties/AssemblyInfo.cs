19418b20fa37a30e1f163378ae2f0c090e374deb
VV <VV@vv>
2017-07-13 01:40:28 +0500

    change name

--- App/App.SocialNetworkServiceProject/Properties/AssemblyInfo.cs
+++ App/App.SocialNetworkServiceProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("App.SocialNetworkServiceProject")]
+[assembly: AssemblyTitle("SocialNetworkServiceProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("App.SocialNetworkServiceProject")]
+[assembly: AssemblyProduct("SocialNetworkServiceProject")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
