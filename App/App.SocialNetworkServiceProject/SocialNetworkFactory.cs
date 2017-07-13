3ba96e3eb7871405bb47c6a7a65f477f42800e32
VV <VV@vv>
2017-07-13 16:14:12 +0500

    deleted all unusing directives

---
+++ App/App.SocialNetworkServiceProject/SocialNetworkFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace App.SocialNetworkService
+{
+    static class SocialNetworkFactory
+    {
+        public enum SocialNetworkEnum
+        {
+            FacebookService,
+            TwitterService,
+            VkontakteService
+        }
+
+    public static ISocialNetworkService CreateInstance(SocialNetworkEnum socialNetwork)
+    {
+        return null;
+    }
+    }
+}
