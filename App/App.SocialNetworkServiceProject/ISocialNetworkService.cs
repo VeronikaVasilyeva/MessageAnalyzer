936e08ff6ccf16b415a591457a064a58bb50c59e
VV <VV@vv>
2017-07-13 14:28:11 +0500

    added summary to ISocialNetworkService

--- App/App.SocialNetworkServiceProject/ISocialNetworkService.cs
+++ App/App.SocialNetworkServiceProject/ISocialNetworkService.cs
@@ -9,12 +9,33 @@ namespace App.SocialNetworkService
 {
     public interface ISocialNetworkService
     {
+        /// <summary>
+        /// Авторизация пользователя. Позволяет работать от имени пользователя
+        /// Подробнее см.документацию Twitter OAuth
+        /// </summary>
+        /// <returns></returns>
         string UserOAuth();
 
+        /// <summary>
+        /// Авторизация приложения. Позволяет совершать действия от имени приложения.
+        /// Подробнее см.документацию Twitter OAuth
+        /// </summary>
+        /// <returns></returns>
         string AppOAuth();
-        
+
+        /// <summary>
+        /// Возвращает последние count постов у username
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
         List<string> GetLastEntries(string username, int count);
 
+        /// <summary>
+        /// Опубликовывает сообщение
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         string PostEntry(string message);
 
     }
